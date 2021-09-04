8e9655257c98de2f5d079a4bddf28d936d52c30f
Daniel Soares <daniel.soares@me.com>
2021-09-04 05:00:02 -0300

    add prisma and next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 export default function Home() {
+  const [session] = useSession()
+
+  function handleAuth() {
+    if (session) {
+      signOut()
+    } else {
+      signIn('github')
+    }
+  }
+
   return (
     <div>
       <h1>Hello</h1>
+
+      <br />
+
+      <button onClick={handleAuth}>{session ? session.user?.name : 'Sign in with GitHub'}</button>
     </div>
   )
 }
